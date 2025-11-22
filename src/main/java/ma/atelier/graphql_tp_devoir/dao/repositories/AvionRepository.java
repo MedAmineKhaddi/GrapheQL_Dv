2338f077d2e2f30b3a4044bf811a360202d87367
Amine <medAmineKhaddi@gmail.com>
2025-11-22 19:41:49 +0100

    add repository : jpa connexion

--- src/main/java/ma/atelier/graphql_tp_devoir/dao/repositories/AvionRepository.java
+++ src/main/java/ma/atelier/graphql_tp_devoir/dao/repositories/AvionRepository.java
@@ -1,9 +1,11 @@
 package ma.atelier.graphql_tp_devoir.dao.repositories;
 
 import ma.atelier.graphql_tp_devoir.dao.entities.Avion;
+import ma.atelier.graphql_tp_devoir.dtos.AvionDtos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface AvionRepository extends JpaRepository<Avion, Long> {
 
+    AvionDtos deleteAvionByMatricul(String matricul);
 }
