641127437d0193c60bd37bca0d58433284d3f117
Amine <medAmineKhaddi@gmail.com>
2025-11-22 19:47:40 +0100

    Change queries

--- src/main/java/ma/atelier/graphql_tp_devoir/GraphQlTpDevoirApplication.java
+++ src/main/java/ma/atelier/graphql_tp_devoir/GraphQlTpDevoirApplication.java
@@ -1,13 +1,34 @@
 package ma.atelier.graphql_tp_devoir;
 
+import ma.atelier.graphql_tp_devoir.dao.entities.Avion;
+import ma.atelier.graphql_tp_devoir.dao.repositories.AvionRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
 
 @SpringBootApplication
 public class GraphQlTpDevoirApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GraphQlTpDevoirApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start (AvionRepository avionRepository) {
+        return args -> {
+
+                Avion avion1=Avion.builder().Id_Avion(1L)
+                        .color("Rouge")
+                        .model("773")
+                        .matricul("2354")
+                        .price(3200.00)
+                        .build();
+                avionRepository.save(avion1);
+
+        };
     }
 
 }
